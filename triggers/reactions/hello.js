60bdb99714a79442e29c323629bebef5acfa3106
Daarka <kasseldom@ymail.com>
2026-03-25 19:25:56 -0700

    embed test

--- triggers/reactions/hello.js
+++ triggers/reactions/hello.js
@@ -17,7 +17,12 @@ module.exports = {
 		// Put all your trigger code over here. This code will be executed when any of the element in the "name" array is found in the message content.
 
 		message.channel.send({
-			content: "Set this trigger response from `./triggers/reactions/hello.js`",
-		});
+  "embeds": [
+    {
+      "description": "test"
+    }
+  ],
+  "content": "test"
+});
 	},
 };
