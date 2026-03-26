93debb2aedab9a550df8cae4614bc6b7cb50c19f
Daarka <kasseldom@ymail.com>
2026-03-25 19:23:06 -0700

    vhtest

--- triggers/reactions/hello.js
+++ triggers/reactions/hello.js
@@ -7,10 +7,11 @@
 
 // For now, the only available property is name array. Not making the name array will result in an error.
 
-/** * @type {import('../../typings').TriggerCommand}
-
+/**
+ * @type {import('../../typings').TriggerCommand}
+ */
 module.exports = {
-	name: ["your", "trigger", "words", "in", "array"],
+	name: ["vhtest"],
 
 	execute(message, args) {
 		// Put all your trigger code over here. This code will be executed when any of the element in the "name" array is found in the message content.
@@ -20,4 +21,3 @@ module.exports = {
 		});
 	},
 };
- */
