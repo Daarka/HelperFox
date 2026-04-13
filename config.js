0920aca1b70f7275ae8d2734aa5ef551b05e0885
Daarka <kasseldom@ymail.com>
2026-04-13 16:38:18 -0700

    delete empty config

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    //////////////////////////
-    token: "", //WRITE YOUR BOT TOKEN
-    botStatus: "Umut Bayraktar ♥", //WRITE YOUR BOT STATUS.
-    //////////////////////////
-}
